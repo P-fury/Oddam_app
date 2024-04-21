1031e4cfee0fdc59e56f3c4a760b943301363567
p-fury <plutocamera@gmail.com>
2024-04-21 12:27:50 +0200

    admin safe delete

--- oddam_app/admin.py
+++ oddam_app/admin.py
@@ -1,11 +1,25 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect
 
 from oddam_app.models import Institution
 from user.models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
-
 # Register your models here.
 
-admin.site.register(CustomUser, UserAdmin)
+# admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Institution)
+
+
+class CustomUserAdmin(UserAdmin):
+    def has_delete_permission(self, request, obj=None):
+        admins_count = CustomUser.objects.filter(is_superuser=True).count()
+        if obj is not None and obj == request.user:
+            return False
+        if admins_count < 2:
+            return False
+        return super().has_delete_permission(request, obj)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
